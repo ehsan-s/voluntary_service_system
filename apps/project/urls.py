da45235cc937c6fa188daf8e760fe554a4cbee2c
ehsan <soltanaghaei97@gmail.com>
2019-02-04 14:06:32 +0330

    Get projects of benefactor

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -3,7 +3,8 @@ from apps.project import views
 
 app_name = "project"
 urlpatterns = [
-    #url(r'^(?P<org>[0-9]+)/posts/$', views.posts, name='get_posts'),
     url(r'^organization/(?P<org_name>.+)/$', views.org_project, name='org_project'),
+    url(r'^benefactor/(?P<benefactor_name>.+)/$', views.benefactor_project, name='benefactor_project'),
+    #url(r'^(?P<org>[0-9]+)/posts/$', views.posts, name='get_posts'),
     #url(r'^login/$', views.LoginView.as_view(), name='login'),
 ]
--- apps/project/views.py
+++ apps/project/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .models import Project, FinancialProject, NonFinancialProject
 from apps.accounts.models import BenefactorProfile, OrganizationProfile, UserProfile
 from django.http import JsonResponse
+from django.db.models import F
 
 
+#  http://127.0.0.1:8000/projects/organization/org/?type=non_financial&status=in_progress
 def org_project(request, org_name):
     if request.method == 'GET':
         type = request.GET.get('type', 'financial')
@@ -14,7 +16,35 @@ def org_project(request, org_name):
                 .filter(organization__profile__user__username=org_name, status=status)\
                 .values('name', 'location', 'deadline', 'money_needed', 'money_donated'))
         elif type == 'non_financial':
-            projects_list = NonFinancialProject.objects\
+            projects_list = list(NonFinancialProject.objects\
                 .filter(organization__profile__user__username=org_name, status=status)\
-                .values('need__category__category', 'need__name', '')
+                .annotate(category=F('need__category__category'), name=F('need__name'),
+                        username=F('benefactor__profile__user__username'))\
+                .values('category', 'name', 'username', 'location'))
         return JsonResponse({'status': '0', 'projects': projects_list})
+    else:
+        return JsonResponse({'status': '-1', 'error': 'this request method is not supported'})
+
+
+def benefactor_project(request, benefactor_name):
+    if request.method == 'GET':
+        type = request.GET.get('type', 'financial')
+        status = request.GET.get('status', 'done')
+        print(benefactor_name, type, status)
+        print(FinancialProject.objects.all()[0].benefactors.all())
+        print(NonFinancialProject.objects.all().values())
+        projects_list = None
+        if type == 'financial':
+            projects_list = list(FinancialProject.objects\
+                .filter(benefactors__profile__user__username__exact=benefactor_name, status=status)\
+                .annotate(username=F('organization__profile__user__username'))\
+                .values('username', 'name', 'location', 'deadline', 'money_needed', 'money_donated'))
+        elif type == 'non_financial':
+            projects_list = list(NonFinancialProject.objects\
+                .filter(benefactor__profile__user__username=benefactor_name, status=status)\
+                .annotate(category=F('need__category__category'), name=F('need__name'),
+                        username=F('organization__profile__user__username'))\
+                .values('category', 'name', 'username', 'location'))
+        return JsonResponse({'status': '0', 'projects': projects_list})
+    else:
+        return JsonResponse({'status': '-1', 'error': 'this request method is not supported'})
