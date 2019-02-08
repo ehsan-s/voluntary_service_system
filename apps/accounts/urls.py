2f05a56244aa6fa9ddeaf7d8883d0d33b71f838b
shayan <shekshaa.75@gmail.com>
2019-02-08 21:13:39 +0330

    update url patterns

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,7 +3,10 @@ from apps.accounts import views
 
 app_name = "accounts"
 urlpatterns = [
-    url(r'^signup-benefactor/', views.register_benefactor, name='benefactor_signup'),
-    url(r'^signup-org/', views.register_organization, name='org_signup'),
-    url(r'^login/$', views.all_login, name='login'),
+    url(r'^benefactor/signup/$', views.register_benefactor, name='benefactor_signup'),
+    url(r'^organization/signup/$', views.register_organization, name='org_signup'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^organization/profile/(?P<username>.+)/$', views.edit_org, name='org_profile'),
+    url(r'^benefactor/profile/(?P<username>.+)/$', views.edit_benefactor, name='benefactor_profile'),
+    url(r'^change_password/(?P<username>.+)/$', views.edit_password, name='edit_password'),
 ]
