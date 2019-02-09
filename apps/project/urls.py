41b211aef41e878a0b275361e94cd721badb719a
shayan <shekshaa.75@gmail.com>
2019-02-09 23:55:52 +0330

    add some url patterns

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -3,10 +3,13 @@ from apps.project import views
 
 app_name = "project"
 urlpatterns = [
+    url(r'^organization/add_financial/(?P<org_name>.+)/$', views.add_financial_project, name='add_financial_project'),
+    url(r'^organization/add_non_financial/(?P<org_name>.+)/$', views.add_non_financial_project, name='add_non_financial_project'),
     url(r'^organization/(?P<org_name>.+)/$', views.org_project, name='org_project'),
     url(r'^benefactor/(?P<benefactor_name>.+)/$', views.benefactor_project, name='benefactor_project'),
+    url(r'^feedbacks/send/$', views.send_feedback, name='feedback'),
     url(r'^feedbacks/organization/(?P<org_name>.+)/$', views.org_feedback, name='org_feedback'),
     url(r'^feedbacks/benefactor/(?P<benefactor_name>.+)/$', views.benefactor_feedback, name='benefactor_feedback'),
     url(r'^requests/organization/(?P<org_name>.+)/$', views.org_request, name='org_request'),
-    url(r'^requests/benefactor/(?P<benefactor_name>.+)/$', views.benefactor_request, name='benefactor_request'),
+    url(r'^requests/benefactor/(?P<benefactor_name>.+)/$', views.benefactor_request, name='benefactor_request')
 ]
