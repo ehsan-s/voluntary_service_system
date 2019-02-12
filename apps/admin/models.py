b44d0579f3f988cda95be41585adcbcf10f822df
shayan <shekshaa.75@gmail.com>
2019-02-12 19:17:02 +0330

    add model of log

--- apps/admin/models.py
+++ apps/admin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Log(models.Model):
+    message = models.CharField(null=False, blank=False, max_length=200)
+    time = models.DateTimeField(default=datetime.now, blank=False)
